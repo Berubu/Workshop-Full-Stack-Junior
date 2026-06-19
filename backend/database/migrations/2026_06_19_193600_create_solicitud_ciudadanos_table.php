7d3721600448dd0880080bad173ccbdfb2abc7f4
DesarrolloAguacate <appguacate@gmail.com>
2026-06-19 13:55:13 -0600

    configuaracion de base de datos y primeras migraciones

---
+++ backend/database/migrations/2026_06_19_193600_create_solicitud_ciudadanos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solicitud_ciudadanos', function (Blueprint $table) {
+            $table->id();
+            $table->string('folio')->unique();
+            $table->string('title',120);
+            $table->text('description');
+            $table->enum('category', ['general', 'especializada']);
+            $table->enum('priority', ['low', 'medium', 'high']);
+            $table->enum('status', ['received', 'in_process', 'resolved', 'rejected']);
+            $table->string('country_code', 2);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('postal_code', 10);
+            $table->string('neighborhood', 100);
+            $table->date('due_date')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solicitud_ciudadanos');
+    }
+};
